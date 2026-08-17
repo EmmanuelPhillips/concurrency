ef1c64726864439825de77449c1a6646536ca0b4
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-17 17:26:12 +0100

    fix race condition using mutex and lock_guard

--- race_condition.cpp
+++ race_condition.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <mutex>
 #include <thread>
 
+std::mutex mtx;
+
 void increment(int &c) {
+  std::lock_guard<std::mutex> lock(mtx); // lock scope (no race condition now)
   for (int i{0}; i < 100000; ++i) {
     ++c;
   }
@@ -9,6 +13,7 @@ void increment(int &c) {
 
 int main() {
   int c{0};
+
   std::thread t1(increment, std::ref(c));
   std::thread t2(increment, std::ref(c));
 
