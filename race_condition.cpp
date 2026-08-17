1619708751aa2c042037bf5eb1875f656f6fc748
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-17 17:20:08 +0100

    demonstrate race condition with unsynchronised shared counter

--- race_condition.cpp
+++ race_condition.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
+#include <thread>
 
-int main() { return 0; }
+void increment(int &c) {
+  for (int i{0}; i < 100000; ++i) {
+    ++c;
+  }
+}
+
+int main() {
+  int c{0};
+  std::thread t1(increment, std::ref(c));
+  std::thread t2(increment, std::ref(c));
+
+  t1.join();
+  t2.join();
+
+  std::cout << "value of c: " << c << '\n';
+
+  return 0;
+}
