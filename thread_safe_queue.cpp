22ae719828aea3c5dfd2143fbfc7dccb9e5a67cd
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-18 11:10:14 +0100

    scaffold thread-safe queue class and task comment, push/pop not yet implemented

---
+++ thread_safe_queue.cpp
@@ -0,0 +1,45 @@
+/*
+ * task: thread-safe queue
+ *
+ * build a queue that multiple threads can safely push to and pop from
+ * at the same time.
+ *
+ * requirements:
+ * - a producer thread pushes integer values into the queue
+ * - a consumer thread pops values out of the queue
+ * - if the queue is empty when the consumer tries to pop, it should block
+ *   (wait efficiently) rather than busy-loop checking repeatedly
+ * - when the producer pushes a new value, the waiting consumer should wake
+ *   up and be able to process it
+ * - the underlying queue data must never be accessed by two threads at
+ *   the same time
+ */
+
+#include <condition_variable>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+class ThreadSafeQueue {
+private:
+  std::queue<int> m_queue{};
+  std::mutex mtx;
+  std::condition_variable cv;
+
+public:
+  void push(int n) {}
+  void pop() {}
+};
+
+int main() {
+  ThreadSafeQueue my_queue{};
+
+  std::thread t1(push, 1);
+  std::thread t2(pop);
+
+  t1.join();
+  t2.join();
+
+  return 0;
+}
