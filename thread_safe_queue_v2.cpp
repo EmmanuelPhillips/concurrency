bf1cb694fe67e8b53f1f29762b3d5d62a797b5d9
RammusOTP <emmanuelphillips33@gmail.com>
2026-08-19 15:56:36 +0100

    reverted main to acutal concurrent version

--- thread_safe_queue_v2.cpp
+++ thread_safe_queue_v2.cpp
@@ -64,16 +64,11 @@ public:
 int main() {
   ThreadSafeQueue tsq{};
 
-  tsq.print_queue();
-
   std::thread t1(&ThreadSafeQueue::produce, &tsq, 5);
-  t1.join();
-
-  tsq.print_queue();
-
   std::thread t2(&ThreadSafeQueue::consume, &tsq);
+
+  t1.join();
   t2.join();
-  tsq.print_queue();
 
   return 0;
 }
