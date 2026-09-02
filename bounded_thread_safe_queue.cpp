a3ad898b8ee88895e21f53c4f26e0a06060a4f18
RammusOTP <emmanuelphillips33@gmail.com>
2026-09-02 16:50:56 +0100

    deleted bad functions. started template again

--- bounded_thread_safe_queue.cpp
+++ bounded_thread_safe_queue.cpp
@@ -4,34 +4,27 @@
 
 class BTSQ {
 private:
-  std::queue<int> data_queue;
-  int max_size;
+  std::queue<int> m_data_queue;
+  int m_size;
 
   std::mutex mtx;
-  std::condition_variable cv;
+  std::condition_variable not_empty;
+  std::condition_variable not_full;
 
   bool done{false};
 
 public:
-  BTSQ(int size) : max_size{size} {}
-  void produce(int n) {
-    std::unique_lock<std::mutex> lock(mtx);
-    if (data_queue.size() >= max_size) {
-      cv.notify_one();
-      return;
-    } else {
-      data_queue.push(n);
-    }
-    cv.notify_one();
-    return;
-  }
+  BTSQ(int size) : m_size{size} {}
+
+  void produce(int n) {}
+  void consume() {}
 };
 
 int main() {
-  BTSQ btsq{5};
+  BTSQ data_queue{5};
 
-  std::thread t1(&BTSQ::produce, &btsq, 5);
-  std::thread t2;
+  std::thread t1(&BTSQ::produce, &data_queue, 5);
+  std::thread t2(&BTSQ::consume, &data_queue);
 
   t1.join();
   t2.join();
