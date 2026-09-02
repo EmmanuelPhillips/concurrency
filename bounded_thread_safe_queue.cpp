4c31226b084584199773401ce1e6d0e7438137ec
RammusOTP <emmanuelphillips33@gmail.com>
2026-09-02 22:58:19 +0100

    removed done bool. used loop and sleep to view consume and produce in action

--- bounded_thread_safe_queue.cpp
+++ bounded_thread_safe_queue.cpp
@@ -1,4 +1,7 @@
+#include <chrono>
+#include <condition_variable>
 #include <iostream>
+#include <mutex>
 #include <queue>
 #include <thread>
 
@@ -10,8 +13,6 @@ private:
   std::mutex mtx;
   std::condition_variable not_empty;
   std::condition_variable not_full;
-
-  bool done{false};
 
 public:
   BTSQ(int size) : m_max_size{size} {}
@@ -29,19 +30,30 @@ public:
   void consume() {
     std::unique_lock<std::mutex> lock(mtx);
 
-    not_empty.wait(lock, [this] { return !(m_data_queue.empty()) || done; });
+    not_empty.wait(lock, [this] { return !(m_data_queue.empty()); });
     int n{m_data_queue.front()};
     m_data_queue.pop();
-    std::cout << "consumed: " << n << " | queue size: " << m_data_queue.size();
+    std::cout << "consumed: " << n << " | queue size: " << m_data_queue.size()
+              << '\n';
     not_full.notify_one();
   }
 };
 
 int main() {
-  BTSQ data_queue{5};
+  BTSQ m_data_queue{5};
 
-  std::thread t1(&BTSQ::produce, &data_queue, 5);
-  std::thread t2(&BTSQ::consume, &data_queue);
+  std::thread t1([&m_data_queue] {
+    for (int i{0}; i < 10; ++i) {
+      m_data_queue.produce(i);
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+  });
+  std::thread t2([&m_data_queue] {
+    for (int i{0}; i < 10; ++i) {
+      m_data_queue.consume();
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+  });
 
   t1.join();
   t2.join();
