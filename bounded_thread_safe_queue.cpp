ee39bf719252337c32c4e44cf56dd64addd12a93
RammusOTP <emmanuelphillips33@gmail.com>
2026-09-02 17:07:45 +0100

    wrote consume function

--- bounded_thread_safe_queue.cpp
+++ bounded_thread_safe_queue.cpp
@@ -5,7 +5,7 @@
 class BTSQ {
 private:
   std::queue<int> m_data_queue;
-  int m_size;
+  int m_max_size;
 
   std::mutex mtx;
   std::condition_variable not_empty;
@@ -14,19 +14,27 @@ private:
   bool done{false};
 
 public:
-  BTSQ(int size) : m_size{size} {}
+  BTSQ(int size) : m_max_size{size} {}
 
   void produce(int n) {
     std::unique_lock<std::mutex> lock(mtx);
 
-    not_full.wait(lock, [this] { return m_data_queue.size() < m_size; });
+    not_full.wait(lock, [this] { return m_data_queue.size() < m_max_size; });
     m_data_queue.push(n);
     std::cout << "produced: " << n << " | queue size: " << m_data_queue.size()
               << '\n';
     not_empty.notify_one();
   }
 
-  void consume() {}
+  void consume() {
+    std::unique_lock<std::mutex> lock(mtx);
+
+    not_empty.wait(lock, [this] { return !(m_data_queue.empty()) || done; });
+    int n{m_data_queue.front()};
+    m_data_queue.pop();
+    std::cout << "consumed: " << n << " | queue size: " << m_data_queue.size();
+    not_full.notify_one();
+  }
 };
 
 int main() {
